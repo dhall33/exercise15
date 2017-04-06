bf6238dee96a76dcd4d3cf91189787de605909b6
dhall33 <ehall33@student.alamo.edu>
2017-04-06 16:15:27 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class Main
+{    
+    public static void main(String[] args)
+    {
+        int limit = 0;
+        int sum   = 0;
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter a Limit: ");
+        limit = scan.nextInt();
+        
+        System.out.println();
+        System.out.println("The sum of the even numbers between 2 and " + limit + " (inclusive) are:");
+        
+        for (int count = 1; count <= limit; count++)
+        {
+            
+            if((count & 1)!= 1)
+            {
+                sum+=count;
+                System.out.println(count);
+            }
+        }
+        System.out.println("The sum is " + sum);
+        
+    }
+}
